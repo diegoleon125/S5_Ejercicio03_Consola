85a339745c06e9fe4b469467f548bb0477723a0e
user <user@LAPTOP-2DK3J1EI>
2023-09-20 18:43:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ingrese el número de notas: ");
+int cantidad = int.Parse(Console.ReadLine());
+float promedio = 0f;
+
+for (int i = 1; i <= cantidad; i++)
+{
+    Console.Write("Ingrese la nota " + i + ": ");
+    int nota = int.Parse(Console.ReadLine());
+    promedio += nota;
+}
+promedio /= cantidad;
+Console.Write("El promedio es " + promedio);
